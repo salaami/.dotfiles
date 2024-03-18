f89a2a9c022087fc00fff195363f37998a37c0d6
Salem Salleh <salem.salleh@getmercury.io>
2024-03-18 19:01:15 +0100

    Replaces bumblebee status bar by polybar.

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+polybar-msg cmd quit
+
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar --reload mybar &
+	done
+else
+	polybar --reload mybar &
+fi
+
+echo "Bar launched..."
